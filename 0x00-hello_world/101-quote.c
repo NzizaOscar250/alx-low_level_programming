e708e0b7520f62de3bb2187278e7befb528604be
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-18 14:14:54 +0200

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+
 /**
- * main - entry point
- *
- *Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 1 (Success)
  */
- 
- int main(void)
- {
- 	printf ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+int main(void)
+{
+	write(STDOUT_FILENO, "and that piece of art is useful
+			\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
- 
- }
+}

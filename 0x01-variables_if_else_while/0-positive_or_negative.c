20806e30539ab2c16ceffedd2b763e648d08d3ac
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-20 11:00:24 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,7 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
-/*
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)

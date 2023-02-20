cb79a9f16533195d839b2382e4be0063fa25857a
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-20 15:58:08 +0200

    fixed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,8 +14,8 @@ int main(void)
 	char letter;
 	for (i = '0'; i <= '9'; i++)
 		putchar(i);
-		for (letter = 'a'; letter <= 'f'; letter++)
-			putchar(letter);
+	for (letter = 'a'; letter <= 'f'; letter++)
+		putchar(letter);
 	putchar('\n');
 	return (0);
 

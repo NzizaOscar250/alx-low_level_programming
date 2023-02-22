998fabba177e2ab9b4fd594554919a076808fa64
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-22 14:19:28 +0200

    uunderstood

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
-
+#include "main.h"
 /**
+ * description : 
  *
- * Description: program to print putchar on the screen
  */
- 
+
 int main(void)
 {
-	printf("_putchar\n");
-	
-	return (0);
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
+    return (0);
 }
+

d9c0d662512debfa04f8cb719bf6fae792d2c90d
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-22 15:20:10 +0200

    error fixed

--- 0x02-functions_nested_loops/3-islower.cpp
+++ 0x02-functions_nested_loops/3-islower.cpp
@@ -1,12 +1,22 @@
+#include "main.h";
 
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int r);
-int print_last_digit(int r);
-void jack_bauer(void);
-void times_table(void);
-int add(int a, int b);
-void print_to_98(int n);
-void print_times_table(int n);
-void putformat(int);
+/**
+ * _islower - Check main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
+ */
+int _islower(int c)
+{
+	char i;
+	int lower = 0;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
+}

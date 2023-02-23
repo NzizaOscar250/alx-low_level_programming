a7d11a7e67ea31a8dbc31829973a5e53c7d0fd1e
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-23 02:52:02 +0200

    trailing space removed

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,9 +3,7 @@
 /**
  * _isalpha - Shows 1 if the input is a
  * letter Another cases, shows 0
- *
  * @c: The character in ASCII code
- *
  * Return: 1 for letters. 0 for the rest.
  */
 int _isalpha(int c)

4d5def2d1e44a257f2b70015b50af4eec07810dc
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-23 02:56:57 +0200

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
- * @c: The character in ASCII code
- * Return: 1 for letters. 0 for the rest.
- */
+ * _isalpha - shows 1 if the input is a letter another cases, shows 0
+ * @n: An input number
+ * Return: 1 for letters. 0 for the rest
+ */
 int _isalpha(int c)
 {
 	char lower, upper;

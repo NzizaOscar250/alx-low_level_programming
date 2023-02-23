6e418fda9e2bd5213d63e1ec5ada2b3e01da5dc4
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-23 04:08:12 +0200

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,26 +1,23 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program
- *
- * This function calls the _putchar function several times to print the
- * characters '_', 'p', 'u', 't', 'c', 'h', 'a', 'r', and a newline character
- * to the standard output. It then returns 0 to indicate successful execution.
- *
- * Return: Always 0 (Success)
+ * main - Entry point
+ * Return: Always 0
  */
+
 int main(void)
+
 {
-    _putchar('_');
-    _putchar('p');
-    _putchar('u');
-    _putchar('t');
-    _putchar('c');
-    _putchar('h');
-    _putchar('a');
-    _putchar('r');
-    _putchar('\n');
 
-    return (0);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+
 }
-

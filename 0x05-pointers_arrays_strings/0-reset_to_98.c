031a52281e84608a75370c92e8cd1178440384af
NzizaOscar250 <santosjuniorsoftwareengineer1@gmail.com>
2023-02-27 14:12:35 +0200

    error fixed

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -11,22 +11,3 @@
  {
  	*n = 98;
  }
-
-
-/**
- * main - the entry point of the program
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-    int n;
-
-    n = 402;
-    printf("n=%d\n", n);
-    reset_to_98(&n);
-    printf("n=%d\n", n);
-    return (0);
-}
-
